4bbfbe477bb06a82889548adf3fb106e048f0a45
vwls <fhbach@gmail.com>
2021-12-16 23:06:49 -0500

    trying to fix failed to generate new lines each minute

--- apps/linesclock/app.js
+++ apps/linesclock/app.js
@@ -9,42 +9,46 @@ let m = d.getMinutes();
 let prevM = m;
 
 function draw() {
-	// Set up the time object
-	d = new Date();
-	h = d.getHours(), m = d.getMinutes();
-	m = d.getMinutes();
-	let time = h + ":" + ("0"+m).substr(-2);
+        // Set up the time object
+        d = new Date();
+        h = d.getHours();
+        m = d.getMinutes();
+        let time = h + ":" + ("0"+m).substr(-2);
+  
+        // Store minute here so we can compare later
+        //prevM = m;
 
-	// Reset the state of the graphics library
-	g.reset();
-	
-	// Clear the area where we want to draw the time
-	g.clearRect(0, 0, 200, 200);
-	
-	// Draw background
-	g.setColor(0, 0, 0);
-	g.fillRect(0, 0, 200, 200);
+        // Reset the state of the graphics library
+        g.reset();
 
-	// Set stroke color
-	g.setColor(255, 255, 255);
-	
-	// Get a new random number every minute
-	if(prevM != m){
-		randomSeed = Math.random(1) * 100;
-	}
+        // Clear the area where we want to draw the time
+        g.clearRect(0, 0, 200, 200);
 
-	// Draw some cool lines
-	for(let i = 0; i < numberOfLines; i+=resolution){
-		g.drawLine(randomSeed, i, i, 100);
-	}
+        // Draw background
+        g.setColor(0, 0, 0);
+        g.fillRect(0, 0, 200, 200);
 
-	// Set font type and size
-	g.setFont("Vector", 40);
+        // Set stroke color
+        g.setColor(255, 255, 255);
+  
+        // Get a new random number every minute
+        if(prevM != m){
+          // a minute has turned over
+          randomSeed = Math.random(1) * 100;
+        }        
 
-	// Display time on screen
-	g.drawString(time, 50, 35);
+        // Draw some cool lines
+        for(let i = 0; i < numberOfLines; i+=resolution){
+                g.drawLine(randomSeed, i, i, 100);
+        }
 
-	prevM = m;
+        // Set font type and size
+        g.setFont("Vector", 40);
+
+        // Display time on screen
+        g.drawString(time, 50, 35);
+  
+        prevM = m;
 }
 
 // Call the draw function every 1 second
